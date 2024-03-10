51a6470e1b949826a1ebb04c33d8f8de11f12a4b
nyanxyz <wl77886@gmail.com>
2024-03-10 20:49:47 +0900

    apply responsive design

--- src/app/paper-card-section.tsx
+++ src/app/paper-card-section.tsx
@@ -2,7 +2,11 @@ import { PaperCard } from "@/app/paper-card";
 
 export function PaperCardSection() {
   return (
-    <div className={"bg-gray-100 py-8 px-12 flex flex-col justify-center"}>
+    <div
+      className={
+        "hidden lg:flex bg-gray-100 py-8 px-12 flex-col justify-center"
+      }
+    >
       <PaperCard />
     </div>
   );
